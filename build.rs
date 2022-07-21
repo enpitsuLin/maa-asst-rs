0f61a54a7035cd338bbbf94220441caccf1badc6
enpitsulin <enpitsulin@gmail.com>
2022-07-21 15:55:25 +0800

    build: 外部连接检索目录使用环境变量`MAA_LIB_PATH`

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@
 use std::env;
 
 fn main() {
-    let libdir = "C:/Users/user/Desktop/MaaBundle-Dev-2022-06-27-08-15-29-de43dd3/";
-    
+    let libdir = env::var("MAA_LIB_PATH").unwrap();
+
     println!("cargo:rustc-flags=-L {}", libdir);
     println!("cargo:rustc-link-search=native={}", libdir);
     let out_dir = env::var("OUT_DIR").unwrap();
