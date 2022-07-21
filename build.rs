d2e5c8f42ee3a9669c9a709d81a6c048c69e3e16
enpitsulin <enpitsulin@gmail.com>
2022-07-21 19:44:03 +0800

    perf: 未设置环境变量提示

--- build.rs
+++ build.rs
@@ -2,9 +2,11 @@
 use std::env;
 
 fn main() {
-    let libdir = env::var("MAA_LIB_PATH").unwrap();
+    let libdir = env::var("MAA_LIB_PATH").unwrap_or_else(|err| {
+        println!("环境变量 MAA_LIB_PATH 未设置");
+        panic!("{:?}", err);
+    });
 
     println!("cargo:rustc-flags=-L {}", libdir);
     println!("cargo:rustc-link-search=native={}", libdir);
-    let out_dir = env::var("OUT_DIR").unwrap();
 }
