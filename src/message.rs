0d3675ac27d8b5d4f799a9b2780daa0be2d324b8
enpitsulin <enpitsulin@gmail.com>
2022-07-23 15:08:17 +0800

    fix: 枚举本身自增无需额外显式设定

--- src/message.rs
+++ src/message.rs
@@ -1,30 +1,31 @@
 /// 回调消息类型枚举
+#[derive(PartialEq)]
 pub enum AsstMsg {
     /* Global Info */
     /// 内部错误
     InternalError = 0,
     /// 初始化失败
-    InitFailed = 1,
+    InitFailed,
     /// 连接相关信息
-    ConnectionInfo = 2,
+    ConnectionInfo,
     /// 全部任务完成
-    AllTasksCompleted = 3,
+    AllTasksCompleted,
     /* TaskChain Info */
     /// 任务链执行/识别错误
     TaskChainError = 10000,
     /// 任务链开始
-    TaskChainStart = 10001,
+    TaskChainStart,
     /// 任务链完成
-    TaskChainCompleted = 10002,
+    TaskChainCompleted,
     /// 任务链额外信息
-    TaskChainExtraInfo = 10003,
+    TaskChainExtraInfo,
     /* SubTask Info */
     /// 原子任务执行/识别错误
     SubTaskError = 20000,
     /// 原子任务开始
-    SubTaskStart = 20001,
+    SubTaskStart,
     /// 原子任务完成
-    SubTaskCompleted = 20002,
+    SubTaskCompleted,
     /// 原子任务额外信息
-    SubTaskExtraInfo = 20003,
+    SubTaskExtraInfo,
 }
