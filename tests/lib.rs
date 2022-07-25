ee6c932f3c1303c09bfa6f01996a4feba10c396e
enpitsulin <enpitsulin@gmail.com>
2022-07-25 18:30:28 +0800

    test: 测试增加覆盖

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,5 +5,8 @@ use maa::*;
 #[test]
 fn test_load_resource() {
     let loaded = load_resource("/some/dir/does/exist").unwrap();
-    assert!(!loaded)
+    assert!(!loaded);
+
+    let loaded = load_resource(env!("MAA_LIB_PATH")).unwrap();
+    assert!(loaded);
 }
