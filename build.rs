4383e947376b42d376ddfe08abe68caf1421f614
enpitsulin <enpitsulin@gmail.com>
2022-07-25 18:35:19 +0800

    perf: 构建目标无需拷贝资源目录

--- build.rs
+++ build.rs
@@ -30,7 +30,7 @@ fn get_output_path() -> PathBuf {
 
 fn find_library_and_copy(path: String, out_dir: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:warning={:?}", out_dir);
-    let dependencies = vec!["/*.dll", "/resource/"];
+    let dependencies = vec!["/*.dll"];
     for item in dependencies.iter() {
         let mut from = path.clone();
         from.push_str(item);
@@ -38,9 +38,7 @@ fn find_library_and_copy(path: String, out_dir: PathBuf) -> Result<(), Box<dyn s
             match entry {
                 Ok(from_file) => {
                     if from_file.is_dir() {
-                        let mut out_dir = out_dir.clone();
-                        out_dir.push("./resource");
-                        copy(from_file, out_dir)?;
+                        panic!("请不要将文件夹命名成*.dll形式")
                     } else {
                         let out_filename = from_file.file_name().unwrap().to_str().unwrap();
                         let mut out_dir = out_dir.clone();
@@ -57,52 +55,5 @@ fn find_library_and_copy(path: String, out_dir: PathBuf) -> Result<(), Box<dyn s
             }
         }
     }
-    Ok(())
-}
-
-pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
-    let mut stack = Vec::new();
-    stack.push(PathBuf::from(from.as_ref()));
-
-    let output_root = PathBuf::from(to.as_ref());
-    let input_root = PathBuf::from(from.as_ref()).components().count();
-
-    while let Some(working_path) = stack.pop() {
-        println!("process: {:?}", &working_path);
-
-        // Generate a relative path
-        let src: PathBuf = working_path.components().skip(input_root).collect();
-
-        // Create a destination if missing
-        let dest = if src.components().count() == 0 {
-            output_root.clone()
-        } else {
-            output_root.join(&src)
-        };
-        if fs::metadata(&dest).is_err() {
-            println!(" mkdir: {:?}", dest);
-            fs::create_dir_all(&dest)?;
-        }
-
-        for entry in fs::read_dir(working_path)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                stack.push(path);
-            } else {
-                match path.file_name() {
-                    Some(filename) => {
-                        let dest_path = dest.join(filename);
-                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
-                        fs::copy(&path, &dest_path)?;
-                    }
-                    None => {
-                        println!("failed: {:?}", path);
-                    }
-                }
-            }
-        }
-    }
-
     Ok(())
 }
