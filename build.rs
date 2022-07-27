e2c18f0f917c130329edcd18137887f3b58ad5ee
enpitsulin <enpitsulin@gmail.com>
2022-07-27 19:51:05 +0800

    perf: 优化库文件打包脚本，使用符号链接
    * 暂未实现#[cfg(not(windows))]

--- build.rs
+++ build.rs
@@ -1,59 +1,62 @@
 #![allow(unused_variables)]
 use std::{
-    env, fs,
+    env, fs, os,
     path::{Path, PathBuf},
 };
 
 fn main() {
-    let libdir = env::var("MAA_LIB_PATH").unwrap_or_else(|err| {
+    let lib_dir = env::var("MAA_LIB_PATH").unwrap_or_else(|err| {
         println!("cargo:warning=环境变量 MAA_LIB_PATH 未设置");
         panic!("{:?}", err);
     });
 
-    println!("cargo:rustc-flags=-L {}", libdir);
-    println!("cargo:rustc-link-search=native={}", libdir);
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    println!("cargo:rustc-flags=-L {}", lib_dir);
+    println!("cargo:rustc-link-search=native={}", lib_dir);
 
     println!("cargo:warning=移动依赖");
-    let output_path = get_output_path();
-    find_library_and_copy(libdir, output_path).unwrap();
+    let work_dir = vec!["../../..", "../../../deps"];
+
+    work_dir.iter().for_each(|dir_path| {
+        let asset_path = Path::new(&lib_dir).join("*.dll");
+        let exe_dir = &Path::new(&out_dir[..])
+            .join(dir_path)
+            .canonicalize()
+            .unwrap();
+        symbolic_link_assets(asset_path.to_str().unwrap(), exe_dir).unwrap();
+    });
 }
 
-fn get_output_path() -> PathBuf {
-    //<root or manifest path>/target/<profile>/
-    let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let build_type = env::var("PROFILE").unwrap();
-    let path = Path::new(&manifest_dir_string)
-        .join("target")
-        .join(build_type);
-    return PathBuf::from(path);
-}
-
-fn find_library_and_copy(path: String, out_dir: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:warning={:?}", out_dir);
-    let dependencies = vec!["/*.dll"];
-    for item in dependencies.iter() {
-        let mut from = path.clone();
-        from.push_str(item);
-        for entry in glob::glob(&from).expect("路径匹配有误") {
-            match entry {
-                Ok(from_file) => {
-                    if from_file.is_dir() {
-                        panic!("请不要将文件夹命名成*.dll形式")
-                    } else {
-                        let out_filename = from_file.file_name().unwrap().to_str().unwrap();
-                        let mut out_dir = out_dir.clone();
-                        out_dir.push(format!("./{}", out_filename).as_str());
-                        let to_file = out_dir;
-                        fs::copy(from_file, to_file.clone()).unwrap_or_else(|err| {
-                            panic!("{:?} {:?}", err, to_file);
-                        });
-                    };
-                }
-                Err(_) => {
-                    todo!()
-                }
+#[cfg(windows)]
+fn symbolic_link_assets(
+    asset_dir: &str,
+    exe_dir: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    for asset in glob::glob(asset_dir).unwrap() {
+        match asset {
+            Ok(dll_origin) => {
+                if dll_origin.is_dir() {
+                    panic!("请不要将文件夹命名成*.dll形式 {:?}", dll_origin)
+                } else {
+                    let dll_filename = dll_origin.file_name().unwrap().to_str().unwrap();
+                    let dll_symbol = exe_dir.join(dll_filename);
+                    if dll_symbol.exists() {
+                        fs::remove_file(dll_symbol.clone())?;
+                    }
+                    os::windows::fs::symlink_file(dll_origin, dll_symbol)?;
+                };
             }
+            Err(_) => unreachable!(),
         }
     }
     Ok(())
+}
+
+#[cfg(not(windows))]
+fn symbolic_link_assets(
+    asset_dir: &str,
+    exe_dir: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    unimplemented!()
 }
