1aa26a76827cb50073b58e49d77969f179678bf3
enpitsulin <enpitsulin@gmail.com>
2025-06-11 00:10:42 +0800

    feat: 打包根据配置

--- crates/maa-gpui/build.rs
+++ crates/maa-gpui/build.rs
@@ -1,9 +1,27 @@
+#![cfg_attr(any(not(target_os = "macos")), allow(unused))]
+
 extern crate embed_resource;
 
+use std::env;
+
 fn main() {
-    let rc_file = std::path::Path::new("res/windows/maa-manifest.rc");
-    println!("cargo:rerun-if-changed={}", rc_file.display());
-    embed_resource::compile(rc_file, embed_resource::NONE)
-        .manifest_required()
-        .unwrap();
+    let target = env::var("CARGO_CFG_TARGET_OS");
+
+    println!("cargo::rustc-check-cfg=cfg(gles)");
+
+    match target.as_deref() {
+        Ok("macos") => {
+            #[cfg(target_os = "macos")]
+            todo!("macos bundle")
+        },
+        #[cfg(target_os = "windows")]
+        Ok("windows") => {
+            let rc_file = std::path::Path::new("res/windows/maa-manifest.rc");
+            println!("cargo:rerun-if-changed={}", rc_file.display());
+            embed_resource::compile(rc_file, embed_resource::NONE)
+                .manifest_required()
+                .unwrap();
+        },
+        _ => (),
+    };
 }
