4641c72f8759bdd0fc04aa13731a92ec5e0c2f05
enpitsulin <enpitsulin@gmail.com>
2025-06-10 22:06:37 +0800

    feat: log 设置

--- crates/maa-gpui/src/main.rs
+++ crates/maa-gpui/src/main.rs
@@ -1,5 +1,5 @@
-use tracing::info;
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use tracing::{info, Level};
+use tracing_subscriber::{filter::Targets, fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 mod app;
 mod assets;
@@ -9,9 +9,25 @@ mod ui;
 
 #[tokio::main]
 async fn main() {
+    let targets_filter = Targets::new().with_targets(vec![("maa_gpui", Level::DEBUG)]);
+
+    let global_env_filter = EnvFilter::try_from_env("MAA_LOG").unwrap_or_else(|_| {
+        #[cfg(debug_assertions)]
+        {
+            EnvFilter::new("debug")
+        }
+        #[cfg(not(debug_assertions))]
+        {
+            EnvFilter::new("warn")
+        }
+    });
+
+    let fmt_layer = fmt::layer();
+
     tracing_subscriber::registry()
-        .with(fmt::layer())
-        .with(EnvFilter::from_env("MAA_LOG"))
+        .with(targets_filter)
+        .with(fmt_layer)
+        .with(global_env_filter)
         .init();
 
     info!("Logger initialized");
