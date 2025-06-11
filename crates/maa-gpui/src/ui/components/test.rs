9c4412b86a38b41bdd497c21218d37d44c17b945
enpitsulin <enpitsulin@gmail.com>
2025-06-11 11:34:06 +0800

    feat: 一些界面调整测试

--- crates/maa-gpui/src/ui/components/test.rs
+++ crates/maa-gpui/src/ui/components/test.rs
@@ -31,55 +31,48 @@ impl_internal_actions!(sidebar_story, [SelectCompany]);
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Item {
-    Playground,
-    Models,
-    Documentation,
+    /// 一键长草
+    Start,
+    // 自动战斗
+    Copilot,
+    /// 小工具
+    Tools,
+    /// 设置
     Settings,
-    DesignEngineering,
-    SalesAndMarketing,
-    Travel,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum SubItem {
-    History,
-    Starred,
-    General,
-    Team,
-    Billing,
-    Limits,
-    Settings,
-    Genesis,
-    Explorer,
-    Quantum,
-    Introduction,
-    GetStarted,
-    Tutorial,
-    Changelog,
+    /// 公招识别
+    Recruitment,
+    /// 干员扫描
+    OperatorsScan,
+    /// 仓库识别
+    DepotScan,
+    /// 视频识别
+    CopilotByVideo,
+    /// 牛牛抽卡
+    GaCha,
+    /// 牛牛监控
+    Peep,
 }
 
 impl Item {
     pub fn label(&self) -> &'static str {
         match self {
-            Self::Playground => "Playground",
-            Self::Models => "Models",
-            Self::Documentation => "Documentation",
-            Self::Settings => "Settings",
-            Self::DesignEngineering => "Design Engineering",
-            Self::SalesAndMarketing => "Sales and Marketing",
-            Self::Travel => "Travel",
+            Self::Start => "一键长草",
+            Self::Copilot => "自动战斗",
+            Self::Tools => "小工具",
+            Self::Settings => "设置",
         }
     }
 
     pub fn icon(&self) -> IconName {
         match self {
-            Self::Playground => IconName::SquareTerminal,
-            Self::Models => IconName::Bot,
-            Self::Documentation => IconName::BookOpen,
+            Self::Start => IconName::SquareTerminal,
+            Self::Copilot => IconName::Bot,
+            Self::Tools => IconName::BookOpen,
             Self::Settings => IconName::Settings2,
-            Self::DesignEngineering => IconName::Frame,
-            Self::SalesAndMarketing => IconName::ChartPie,
-            Self::Travel => IconName::Map,
         }
     }
 
@@ -101,15 +94,14 @@ impl Item {
 
     pub fn items(&self) -> Vec<SubItem> {
         match self {
-            Self::Playground => vec![SubItem::History, SubItem::Starred, SubItem::Settings],
-            Self::Models => vec![SubItem::Genesis, SubItem::Explorer, SubItem::Quantum],
-            Self::Documentation => vec![
-                SubItem::Introduction,
-                SubItem::GetStarted,
-                SubItem::Tutorial,
-                SubItem::Changelog,
+            Self::Tools => vec![
+                SubItem::Recruitment,
+                SubItem::OperatorsScan,
+                SubItem::DepotScan,
+                SubItem::CopilotByVideo,
+                SubItem::GaCha,
+                SubItem::Peep,
             ],
-            Self::Settings => vec![SubItem::General, SubItem::Team, SubItem::Billing, SubItem::Limits],
             _ => Vec::new(),
         }
     }
@@ -118,20 +110,12 @@ impl Item {
 impl SubItem {
     pub fn label(&self) -> &'static str {
         match self {
-            Self::History => "History",
-            Self::Starred => "Starred",
-            Self::Settings => "Settings",
-            Self::Genesis => "Genesis",
-            Self::Explorer => "Explorer",
-            Self::Quantum => "Quantum",
-            Self::Introduction => "Introduction",
-            Self::GetStarted => "Get Started",
-            Self::Tutorial => "Tutorial",
-            Self::Changelog => "Changelog",
-            Self::Team => "Team",
-            Self::Billing => "Billing",
-            Self::Limits => "Limits",
-            Self::General => "General",
+            Self::Recruitment => "公招识别",
+            Self::OperatorsScan => "干员扫描",
+            Self::DepotScan => "仓库识别",
+            Self::CopilotByVideo => "视频识别",
+            Self::GaCha => "牛牛抽卡",
+            Self::Peep => "牛牛监控",
         }
     }
 
@@ -166,7 +150,7 @@ impl TestView {
     pub fn new(_window: &mut Window, _cx: &mut App) -> Self {
         Self {
             active_items: HashMap::new(),
-            last_active_item: Item::Playground,
+            last_active_item: Item::Start,
             active_subitem: None,
             checked: false,
         }
@@ -186,6 +170,121 @@ impl TestView {
                     vec![render_ok(window, cx), render_cancel(window, cx)]
                 })
         });
+    }
+
+    fn render_sidebar(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let groups: [Vec<Item>; 2] = [
+            vec![Item::Start, Item::Copilot, Item::Tools],
+            vec![Item::Settings],
+        ];
+
+        Sidebar::left()
+            .collapsed(cx.sidebar_collapsed())
+            .header(
+                SidebarHeader::new()
+                    .w_full()
+                    .child(
+                        div()
+                            .flex()
+                            .items_center()
+                            .justify_center()
+                            .rounded_md()
+                            .bg(blue_500())
+                            .text_color(white())
+                            .size_8()
+                            .flex_shrink_0()
+                            .when(!cx.sidebar_collapsed(), |this| {
+                                this.child(Icon::new(IconName::GalleryVerticalEnd))
+                            })
+                            .when(cx.sidebar_collapsed(), |this| {
+                                this.size_4()
+                                    .bg(cx.theme().transparent)
+                                    .text_color(cx.theme().foreground)
+                                    .child(Icon::new(IconName::GalleryVerticalEnd))
+                            }),
+                    )
+                    .when(!cx.sidebar_collapsed(), |this| {
+                        this.child(
+                            v_flex()
+                                .gap_0()
+                                .text_sm()
+                                .flex_1()
+                                .line_height(relative(1.25))
+                                .overflow_hidden()
+                                .text_ellipsis()
+                                .child("Company Name")
+                                .child(div().child("Enterprise").text_xs()),
+                        )
+                    })
+                    .when(!cx.sidebar_collapsed(), |this| {
+                        this.child(Icon::new(IconName::ChevronsUpDown).size_4().flex_shrink_0())
+                    })
+                    .popup_menu(|menu, _, _| {
+                        menu.menu(
+                            "Twitter Inc.",
+                            Box::new(SelectCompany(SharedString::from("twitter"))),
+                        )
+                        .menu(
+                            "Meta Platforms",
+                            Box::new(SelectCompany(SharedString::from("meta"))),
+                        )
+                        .menu(
+                            "Google Inc.",
+                            Box::new(SelectCompany(SharedString::from("google"))),
+                        )
+                    }),
+            )
+            .child(
+                SidebarGroup::new("Platform").child(SidebarMenu::new().children(groups[0].iter().map(
+                    |item| {
+                        SidebarMenuItem::new(item.label())
+                            .icon(item.icon())
+                            .active(self.active_items.contains_key(item))
+                            .children(item.items().into_iter().enumerate().map(|(ix, sub_item)| {
+                                SidebarMenuItem::new(sub_item.label())
+                                    .active(self.active_subitem == Some(sub_item))
+                                    .when(ix == 0, |this| {
+                                        this.suffix(
+                                            Switch::new("switch").xsmall().checked(self.checked).on_click(
+                                                cx.listener(|this, checked, _, _| this.checked = *checked),
+                                            ),
+                                        )
+                                    })
+                                    .on_click(cx.listener(sub_item.handler(&item)))
+                            }))
+                            .on_click(cx.listener(item.handler()))
+                    },
+                ))),
+            )
+            .child(SidebarGroup::new("Projects").child(SidebarMenu::new().children(
+                groups[1].iter().enumerate().map(|(ix, item)| {
+                    SidebarMenuItem::new(item.label())
+                        .icon(item.icon())
+                        .active(self.last_active_item == *item)
+                        .when(ix == 0, |this| {
+                            this.suffix(
+                                Badge::new()
+                                    .dot()
+                                    .count(1)
+                                    .child(div().p_0p5().child(Icon::new(IconName::Bell))),
+                            )
+                        })
+                        .when(ix == 1, |this| this.suffix(IconName::Settings2))
+                }),
+            )))
+            .footer(
+                SidebarFooter::new()
+                    .justify_between()
+                    .child(
+                        h_flex()
+                            .gap_2()
+                            .child(IconName::CircleUser)
+                            .when(!cx.sidebar_collapsed(), |this| this.child("Jason Lee")),
+                    )
+                    .when(!cx.sidebar_collapsed(), |this| {
+                        this.child(Icon::new(IconName::ChevronsUpDown).size_4())
+                    }),
+            )
     }
 
     fn render_content(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
@@ -218,133 +317,35 @@ impl TestView {
                     .on_click(cx.listener(|this, _, window, cx| this.show_modal(window, cx))),
             )
     }
+
+    fn render_breadcrumb(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        Breadcrumb::new()
+            .item(
+                BreadcrumbItem::new("0", "Home").on_click(cx.listener(|this, _, _, cx| {
+                    this.last_active_item = Item::Start;
+                    cx.notify();
+                })),
+            )
+            .item(
+                BreadcrumbItem::new("1", self.last_active_item.label()).on_click(cx.listener(
+                    |this, _, _, cx| {
+                        this.active_subitem = None;
+                        cx.notify();
+                    },
+                )),
+            )
+            .when_some(self.active_subitem, |this, subitem| {
+                this.item(BreadcrumbItem::new("2", subitem.label()))
+            })
+    }
 }
 
 impl Render for TestView {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
-        let groups: [Vec<Item>; 2] = [
-            vec![
-                Item::Playground,
-                Item::Models,
-                Item::Documentation,
-                Item::Settings,
-            ],
-            vec![Item::DesignEngineering, Item::SalesAndMarketing, Item::Travel],
-        ];
-
         h_flex()
             .rounded(cx.theme().radius)
             .h_full()
-            .child(
-                Sidebar::left()
-                    .collapsed(cx.sidebar_collapsed())
-                    .header(
-                        SidebarHeader::new()
-                            .w_full()
-                            .child(
-                                div()
-                                    .flex()
-                                    .items_center()
-                                    .justify_center()
-                                    .rounded_md()
-                                    .bg(blue_500())
-                                    .text_color(white())
-                                    .size_8()
-                                    .flex_shrink_0()
-                                    .when(!cx.sidebar_collapsed(), |this| {
-                                        this.child(Icon::new(IconName::GalleryVerticalEnd))
-                                    })
-                                    .when(cx.sidebar_collapsed(), |this| {
-                                        this.size_4()
-                                            .bg(cx.theme().transparent)
-                                            .text_color(cx.theme().foreground)
-                                            .child(Icon::new(IconName::GalleryVerticalEnd))
-                                    }),
-                            )
-                            .when(!cx.sidebar_collapsed(), |this| {
-                                this.child(
-                                    v_flex()
-                                        .gap_0()
-                                        .text_sm()
-                                        .flex_1()
-                                        .line_height(relative(1.25))
-                                        .overflow_hidden()
-                                        .text_ellipsis()
-                                        .child("Company Name")
-                                        .child(div().child("Enterprise").text_xs()),
-                                )
-                            })
-                            .when(!cx.sidebar_collapsed(), |this| {
-                                this.child(Icon::new(IconName::ChevronsUpDown).size_4().flex_shrink_0())
-                            })
-                            .popup_menu(|menu, _, _| {
-                                menu.menu(
-                                    "Twitter Inc.",
-                                    Box::new(SelectCompany(SharedString::from("twitter"))),
-                                )
-                                .menu(
-                                    "Meta Platforms",
-                                    Box::new(SelectCompany(SharedString::from("meta"))),
-                                )
-                                .menu(
-                                    "Google Inc.",
-                                    Box::new(SelectCompany(SharedString::from("google"))),
-                                )
-                            }),
-                    )
-                    .child(SidebarGroup::new("Platform").child(SidebarMenu::new().children(
-                        groups[0].iter().map(|item| {
-                            SidebarMenuItem::new(item.label())
-                                .icon(item.icon())
-                                .active(self.active_items.contains_key(item))
-                                .children(item.items().into_iter().enumerate().map(|(ix, sub_item)| {
-                                    SidebarMenuItem::new(sub_item.label())
-                                        .active(self.active_subitem == Some(sub_item))
-                                        .when(ix == 0, |this| {
-                                            this.suffix(
-                                                Switch::new("switch")
-                                                    .xsmall()
-                                                    .checked(self.checked)
-                                                    .on_click(cx.listener(|this, checked, _, _| {
-                                                        this.checked = *checked
-                                                    })),
-                                            )
-                                        })
-                                        .on_click(cx.listener(sub_item.handler(&item)))
-                                }))
-                                .on_click(cx.listener(item.handler()))
-                        }),
-                    )))
-                    .child(SidebarGroup::new("Projects").child(SidebarMenu::new().children(
-                        groups[1].iter().enumerate().map(|(ix, item)| {
-                            SidebarMenuItem::new(item.label())
-                                .icon(item.icon())
-                                .active(self.last_active_item == *item)
-                                .when(ix == 0, |this| {
-                                    this.suffix(
-                                        Badge::new()
-                                            .dot()
-                                            .count(1)
-                                            .child(div().p_0p5().child(Icon::new(IconName::Bell))),
-                                    )
-                                })
-                                .when(ix == 1, |this| this.suffix(IconName::Settings2))
-                        }),
-                    )))
-                    .footer(
-                        SidebarFooter::new()
-                            .justify_between()
-                            .child(
-                                h_flex()
-                                    .gap_2()
-                                    .child(IconName::CircleUser)
-                                    .when(!cx.sidebar_collapsed(), |this| this.child("Jason Lee")),
-                            )
-                            .when(!cx.sidebar_collapsed(), |this| {
-                                this.child(Icon::new(IconName::ChevronsUpDown).size_4())
-                            }),
-                    ),
-            )
+            .child(self.render_sidebar(window, cx))
             .child(
                 v_flex()
                     .size_full()
@@ -363,24 +364,7 @@ impl Render for TestView {
                                     })),
                             )
                             .child(Divider::vertical().h_4())
-                            .child(
-                                Breadcrumb::new()
-                                    .item(BreadcrumbItem::new("0", "Home").on_click(cx.listener(
-                                        |this, _, _, cx| {
-                                            this.last_active_item = Item::Playground;
-                                            cx.notify();
-                                        },
-                                    )))
-                                    .item(BreadcrumbItem::new("1", self.last_active_item.label()).on_click(
-                                        cx.listener(|this, _, _, cx| {
-                                            this.active_subitem = None;
-                                            cx.notify();
-                                        }),
-                                    ))
-                                    .when_some(self.active_subitem, |this, subitem| {
-                                        this.item(BreadcrumbItem::new("2", subitem.label()))
-                                    }),
-                            ),
+                            .child(self.render_breadcrumb(window, cx)),
                     )
                     .child(self.render_content(window, cx)),
             )
