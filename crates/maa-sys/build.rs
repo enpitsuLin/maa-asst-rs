0ce65bd96cd4e7564dda54793a2fd38b9d3ac156
enpitsulin <enpitsulin@gmail.com>
2025-06-13 16:10:16 +0800

    build: build 脚本移除 link 搜索

--- crates/maa-sys/build.rs
+++ crates/maa-sys/build.rs
@@ -5,19 +5,9 @@ use std::{env, path::PathBuf};
 extern crate bindgen;
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=MAA_LIB_PATH");
     println!("cargo:rerun-if-env-changed=MAA_HEADER_PATH");
 
-    let lib_dir = env::var("MAA_LIB_PATH").unwrap_or_else(|err| {
-        println!("cargo:warning=环境变量 MAA_LIB_PATH 未设置");
-        panic!("{:?}", err);
-    });
-
     let out_dir = env::var("OUT_DIR").unwrap();
-
-    println!("cargo:rustc-link-search=native={}", lib_dir);
-
-    println!("cargo:rustc-link-lib=dylib=MaaCore");
 
     let maa_header_path = env::var("MAA_HEADER_PATH").unwrap();
 
